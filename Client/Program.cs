9dad2d8e0de85732654496d5517055ff6e343092
Owen Daly-Furlong <owendalyfurlong@protonmail.com>
2020-10-24 16:36:46 +0100

    Get greeter service running

---
+++ Client/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using Server;
+
+namespace Client
+{
+    public class Program
+    {
+        static async Task Main()
+        {
+            using var channel = GrpcChannel.ForAddress("http://localhost:5000");
+
+            Console.WriteLine(
+                (await new Greeter.GreeterClient(channel)
+                    .SayHelloAsync(new HelloRequest {Name = "Owen"}))
+                .Message);
+        }
+    }
+}
